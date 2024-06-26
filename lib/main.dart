548433b745b3f3244a199030f1b210d4e3993a2b
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-27 03:18:23 +0500

    Added navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:chat_app_test/pages/login_page.dart';
+import 'package:chat_app_test/constants/colors.dart';
+import 'package:chat_app_test/services/auth_service.dart';
+import 'package:chat_app_test/services/navigation_service.dart';
 import 'package:chat_app_test/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -16,16 +18,26 @@ Future<void> setup() async {
 
 class MyApp extends StatelessWidget {
   final GetIt _getIt = GetIt.instance;
-  MyApp({super.key}) {}
+  late NavigationService _navigationService;
+  late AuthService _authService;
+  MyApp({super.key}) {
+    _navigationService = _getIt.get<NavigationService>();
+    _authService = _getIt.get<AuthService>();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigationService.navigatorKey,
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: ColorSelect.primaryColor,
+        ),
         useMaterial3: true,
         fontFamily: 'Gilroy',
       ),
-      home: LoginPage(),
+      initialRoute: _authService.user != null ? "/home" : "/login",
+      routes: _navigationService.routes,
     );
   }
 }
