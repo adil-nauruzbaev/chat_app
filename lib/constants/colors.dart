780954ee5bf626a7f180dab44c7736ad1ee0a0d0
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-26 22:59:30 +0500

    Constants setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorSelect {
+  static const primaryColor = Color.fromRGBO(60, 237, 120, 1);
+  static const errorColor = Color.fromRGBO(250, 16, 8, 1);
+  static const primaryText = Color.fromRGBO(0, 0, 0, 1);
+  static const secondaryText = Color.fromRGBO(94, 122, 144, 1);
+  static const primaryLabel = Color.fromRGBO(43, 51, 62, 1);
+}
