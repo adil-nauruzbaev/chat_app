61014760ddc9aa58b3d0daa1470e2d0333a6911a
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-27 03:17:17 +0500

    Registered services

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,7 @@
 import 'package:chat_app_test/firebase_options.dart';
+import 'package:chat_app_test/services/alert_service.dart';
+import 'package:chat_app_test/services/auth_service.dart';
+import 'package:chat_app_test/services/navigation_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,4 +13,13 @@ Future<void> setupFirebase() async {
 
 Future<void> registeredServices() async {
   final GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<AuthService>(
+    AuthService(),
+  );
+  getIt.registerSingleton<NavigationService>(
+    NavigationService(),
+  );
+  getIt.registerSingleton<AlertService>(
+    AlertService(),
+  );
 }
