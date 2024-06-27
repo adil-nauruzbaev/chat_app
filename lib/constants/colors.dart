8772c0538499af51e2f9c4b0e11dad103a4a8ba9
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-27 05:02:39 +0500

    Added some colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class ColorSelect {
   static const primaryColor = Color.fromRGBO(60, 237, 120, 1);
+  static const secondaryColor = Color.fromRGBO(237, 242, 246, 1);
   static const errorColor = Color.fromRGBO(250, 16, 8, 1);
   static const primaryText = Color.fromRGBO(0, 0, 0, 1);
   static const secondaryText = Color.fromRGBO(94, 122, 144, 1);
   static const primaryLabel = Color.fromRGBO(43, 51, 62, 1);
+  static const secondaryLabel = Color.fromRGBO(157, 183, 203, 1);
 }
