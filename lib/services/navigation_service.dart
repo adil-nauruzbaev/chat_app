7888da3f7a437e28bdbde260161b200763608253
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-27 05:03:49 +0500

    Added routes

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,10 +1,14 @@
+import 'package:chat_app_test/pages/home_page.dart';
 import 'package:chat_app_test/pages/login_page.dart';
+import 'package:chat_app_test/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 class NavigationService {
   late GlobalKey<NavigatorState> _navigationKey;
   final Map<String, Widget Function(BuildContext)> _routes = {
     "/login": (context) => const LoginPage(),
+    "/register": (context) => const RegisterPage(),
+    "/home": (context) => const HomePage(),
   };
 
   GlobalKey<NavigatorState>? get navigatorKey {
