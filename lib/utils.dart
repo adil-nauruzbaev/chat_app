50f637ca8ac3cf9d41904838425d72f5448d607d
Adil Nauruzbaev <aresbitch@mail.ru>
2024-06-27 05:02:33 +0500

    Generation chat_id

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,10 @@
 import 'package:chat_app_test/firebase_options.dart';
 import 'package:chat_app_test/services/alert_service.dart';
 import 'package:chat_app_test/services/auth_service.dart';
+import 'package:chat_app_test/services/database_service.dart';
+import 'package:chat_app_test/services/media_service.dart';
 import 'package:chat_app_test/services/navigation_service.dart';
+import 'package:chat_app_test/services/storage_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 
@@ -22,4 +25,20 @@ Future<void> registeredServices() async {
   getIt.registerSingleton<AlertService>(
     AlertService(),
   );
+  getIt.registerSingleton<MediaService>(
+    MediaService(),
+  );
+  getIt.registerSingleton<StorageService>(
+    StorageService(),
+  );
+  getIt.registerSingleton<DatabaseService>(
+    DatabaseService(),
+  );
+}
+
+String generateChatID({required String uid1, required String uid2}) {
+  List uids = [uid1, uid2];
+  uids.sort();
+  String chatID = uids.fold('', (id, uid) => "$id$uid");
+  return chatID;
 }
